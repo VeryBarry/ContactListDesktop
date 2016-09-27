9f4af9923824453a631e93e93651396b8a886d18
VeryBarry <barry.a.daniels@gmail.com>
2016-09-27 16:31:31 -0400

    JSON broken

--- src/ContactList/Controller.java
+++ src/ContactList/Controller.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
--- src/ContactList/JsonSaveRead.java
+++ src/ContactList/JsonSaveRead.java
@@ -11,23 +11,19 @@ import java.io.IOException;
  * Created by VeryBarry on 9/27/16.
  */
 public class JsonSaveRead {
-    public static void main(String[] args) throws IOException {
 
-
-        JsonSerializer serializer = new JsonSerializer();
-        String json = serializer.serialize(p);
-        FileWriter fw = new FileWriter(f);
+    JsonSerializer serializer = new JsonSerializer();
+    String json = serializer.serialize(p);
+    FileWriter fw = new FileWriter(f);
         fw.write(json);
         fw.close();
 
-        FileReader fr = new FileReader(f);
-        int fileSize = (int) f.length();
-        char[] contents = new char[fileSize];
+    FileReader fr = new FileReader(f);
+    int fileSize = (int) f.length();
+    char[] contents = new char[fileSize];
         fr.read(contents, 0, fileSize);
-        JsonParser parser = new JsonParser();
-        Contact c = parser.parse(contents, Contact.class);
+    JsonParser parser = new JsonParser();
+    Contact c = parser.parse(contents, Contact.class);
         System.out.println(c);
 
-
-    }
 }

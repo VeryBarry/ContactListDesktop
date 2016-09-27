c6569140d7aa8e5094514afffadb17b0d4ddd82b
VeryBarry <barry.a.daniels@gmail.com>
2016-09-27 17:27:41 -0400

    json saves empty files

--- src/ContactList/Controller.java
+++ src/ContactList/Controller.java
@@ -6,7 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
@@ -33,6 +40,7 @@ public class Controller implements Initializable{
                     nameTextF.clear();
                     phoneTextF.clear();
                     emailTextF.clear();
+                    saveContactList(contact, FILE_NAME);
                 }
             }
         }
@@ -45,6 +53,37 @@ public class Controller implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+       // contacts.addAll(<Collection>);
         contactListView.setItems(contacts);
+    }
+
+    static String FILE_NAME = "ContactList.json";
+
+    public ArrayList<Contact> loadContactList(String fileName) {
+        File f = new File(fileName);
+        try{
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] contents = new char[fileSize];
+            fr.read(contents, 0, fileSize);
+            JsonParser parser = new JsonParser();
+            return parser.parse(contents, Contact.class);
+        } catch (Exception e) {
+            System.out.println("Couldn't load file!");
+            return null;
+        }
+    }
+
+    public void saveContactList(Contact contact, String fileName)  {
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.deep(true).serialize(contact);
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(json);
+            fw.close();
+        }catch (Exception e) {
+            System.out.println("Couldn't save to file!");
+        }
     }
 }
